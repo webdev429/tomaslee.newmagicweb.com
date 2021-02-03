bcadbcb4979857cc51b4a9c2a6639aac6db1b643
webdev429 <arman.x429@gmail.com>
2021-02-03 15:08:35 -0500

    Initialize portfolio

---
+++ send.php
@@ -0,0 +1,11 @@
+<?php $name = $_POST['contactName'];
+$email = $_POST['contactEmail'];
+echo $name;
+$subject = $_POST['contactSubject'];
+$message = $_POST['contactMessage'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "zc.web429@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
