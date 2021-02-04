f3b9ea360c030b00fb45d898291e94a7e6940997
webdev429 <arman.x429@gmail.com>
2021-02-04 04:48:03 -0500

    updated portfolio with footer

--- send.php
+++ send.php
@@ -4,7 +4,7 @@ echo $name;
 $subject = $_POST['contactSubject'];
 $message = $_POST['contactMessage'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "zc.web429@gmail.com";
+$recipient = "web.dev429@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
